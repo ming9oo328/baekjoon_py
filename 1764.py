6e10eedaab05b847361a39c39898fdf8f1887bc2
ming9oo <mj10050203@gmail.com>
2023-01-28 00:03:15 +0900

    3주차 마지막!!

--- 1764.py
+++ 1764.py
@@ -1,28 +1,11 @@
-'''
-N,M=map(int,input().split())
-a=[];b=[];c=[]
+N,M = map(int,input().split())
+nohear = set()
+nosee = set()
 for i in range(N):
-    a.append(input())
+    nohear.add(input())
 for i in range(M):
-    b.append(input())
-for i in b:
-    if i in a:
-        c.append(i)
-print(len(c))
-print(*c,sep='\n')
-
-#sys.stdin.readline()
-'''
-N,M=map(int,input().split())
-a=set()
-b=set()
-
-for i in range(N):
-    a.add(input())
-for i in range(M):
-    B=input()
-    b.add(input())
-c=sorted(list(a & b))
-print(len(c))
-for i in c:
-    print(c)
+    nosee.add(input())
+same = sorted(list(nohear & nosee))
+print(len(same))
+for i in same:
+    print(i)
