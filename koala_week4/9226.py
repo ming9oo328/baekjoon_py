2795d6dbf2a6a88273415e63f3a397092e25f7ce
ming9oo <mj10050203@gmail.com>
2023-02-12 14:29:48 +0900

    9226수정

--- koala_week4/9226.py
+++ koala_week4/9226.py
@@ -1,18 +1,39 @@
+'''
 moeum = ['a','e','i','o','u']
 while True:
     word = input()
     newword = []
+    stop = 0
     if word == "#":
         break
-    word = list(word)
     for i in range(len(word)):
-        if word[i] in moeum:
-            tmp = 0
-            for j in range(i,len(word)):
-                newword.insert(tmp,word[j])
-                tmp+=1
-            newword.append("ay")
-        else:
+        while (word[i] in moeum) == False:
             newword.append(word[i])
+            stop += i
+    tmp = 0
+    print(*newword)
+    for j in range(stop,len(word)):
+        newword.insert(tmp,word[j])
+        tmp+=1
+    newword.append("ay")
+
     for i in range(len(newword)):
         print(newword[i],end ='')
+    print()
+'''
+
+moeum = ['a','e','i','o','u']
+while True:
+    word = list(input())
+    if word[0] == "#":
+        break
+    for i in range(len(word)):
+        if word[0] in moeum:
+            break
+        else:
+            word+=word[0]
+            del word[0]
+    word+="ay"
+    for i in range(len(word)):
+        print(word[i],end ='')
+    print()
